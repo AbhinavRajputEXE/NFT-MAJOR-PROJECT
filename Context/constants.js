14140f375ba694bb82fb2c15fd51643863162d3c
Abhinav <abhinavrajputaes@gmail.com>
2024-05-14 22:18:06 +0530

    Major commit

---
+++ Context/constants.js
@@ -0,0 +1,8 @@
+import nftMarketplace from "./NFTMarketplace.json";
+
+//NFT MARKETPLACE
+export const NFTMarketplaceAddress =
+  "0x3045BBcFD3322a0d678E7834d9B149c44914afA4";
+export const NFTMarketplaceABI = nftMarketplace.abi;
+
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
